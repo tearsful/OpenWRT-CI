a8237c57c4da454e3e1a8804ed72a058f71f0a66
tearsful <110714634+tearsful@users.noreply.github.com>
2024-09-02 23:02:08 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -8,6 +8,9 @@ sed -i "s/\.ssid=.*/\.ssid=$WRT_WIFI/g" $(find ./package/kernel/mac80211/ ./pack
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
+cp -rf $GITHUB_WORKSPACE/banner package/base-files/files/etc
+chmod +x package/base-files/files/etc/*
+
 CFG_FILE="./package/base-files/files/bin/config_generate"
 #修改默认IP地址
 sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $CFG_FILE
