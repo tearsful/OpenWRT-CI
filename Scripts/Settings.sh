486205a70ae29a023d8bd1a72f3e84872826b6f6
tearsful <110714634+tearsful@users.noreply.github.com>
2024-09-04 23:41:44 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -30,7 +30,7 @@ if [[ $WRT_REPO == *"lede"* ]]; then
 	#修改默认时间格式
 	sed -i 's/os.date()/os.date("%Y-%m-%d %H:%M:%S %A")/g' $LEDE_FILE
 	#添加编译日期标识
-	sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/Openwrt\/ 布丁智能科技 © 蓝色的海 compiled in $WRT_DATE/g" $LEDE_FILE
+	CURRENT_TIME=$(date +"%Y-%m-%d") && sed -i "s/(\(<%=pcdata(ver.luciversion)%>\))/Openwrt\/ 布丁智能科技 © 蓝色的海 compiled in $CURRENT_TIME/g" $LEDE_FILE
 else
 	#修改immortalwrt.lan关联IP
 	sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
