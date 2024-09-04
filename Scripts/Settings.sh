fb383d6b6ebaf650ac71ecbd76ee1d84f4e5d8e1
tearsful <110714634+tearsful@users.noreply.github.com>
2024-09-04 21:58:25 +0800

    Update Settings.sh

--- Scripts/Settings.sh
+++ Scripts/Settings.sh
@@ -35,8 +35,7 @@ else
 	#修改immortalwrt.lan关联IP
 	sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 	#添加编译日期标识
-	sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ 布丁智能科技 © 蓝色的海 compiled in $WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
-
+	CURRENT_TIME=$(date +"%Y-%m-%d %H:%M:%S") && sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ 布丁智能科技 © 蓝色的海 compiled in $CURRENT_TIME')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
 fi
 
 #配置文件修改
