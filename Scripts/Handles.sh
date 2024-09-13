4761b7a728cae83fcd82cadf1a61d287bd81d7b4
tearsful <110714634+tearsful@users.noreply.github.com>
2024-09-13 22:25:26 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -73,3 +73,8 @@ if [ -f "$SP_FILE" ]; then
 	cd $PKG_PATCH && echo "ssr-plus has been fixed!"
 fi
 
+#mosdns
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
